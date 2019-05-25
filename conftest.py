00c3b367694db954ec6a1bfe1cd61cc797f9b281
ocherniavskyi <chernyavskiy.alex@gmail.com>
2019-05-26 01:19:07 +0300

    allure screenshots maker added

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
 from selene import config
+import allure
+from allure import attachment_type
+from selene import browser
 
 from api.jira_client.clients.JiraClient import JiraClient
 
@@ -8,3 +11,12 @@ password = 'OleksiiCherniavskyi'
 project = 'WEBINAR'
 issue_tag = 'ocher'
 config.base_url = jira_url
+config.reports_folder = './reports'
+
+
+def pytest_exception_interact(node, call, report):
+    allure.attach(
+        name='Screenshot',
+        contents=browser.driver().get_screenshot_as_png(),
+        type=attachment_type.PNG
+    )
